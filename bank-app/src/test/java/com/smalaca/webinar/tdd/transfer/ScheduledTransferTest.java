ab78c83ec0887a1e7c4303c151ac2bc1cd8469e6
smalaca <ab13.krakow@gmail.com>
2020-07-01 14:15:24 +0200

    ScheduledTransfer requirements

---
+++ bank-app/src/test/java/com/smalaca/webinar/tdd/transfer/ScheduledTransferTest.java
@@ -0,0 +1,21 @@
+package com.smalaca.webinar.tdd.transfer;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+class ScheduledTransferTest {
+    /*
+    REQUIREMENTS:
+    1. Register scheduled transfer with information:
+        - unique id of the transfer (<accountFrom>-<accountTo>-<name>)
+        - name of the transfer
+        - account from
+        - account to
+        - money to transfer
+    2. Throw exception if there's no account for account number
+     */
+}
