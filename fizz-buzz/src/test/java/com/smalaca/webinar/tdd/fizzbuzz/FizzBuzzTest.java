0921f9334db60b09632c6bbcfab0c73f1cfe46fe
smalaca <ab13.krakow@gmail.com>
2020-07-01 13:34:00 +0200

    FizzBuzz requirements

---
+++ fizz-buzz/src/test/java/com/smalaca/webinar/tdd/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package com.smalaca.webinar.tdd.fizzbuzz;
+
+class FizzBuzzTest {
+    /*
+      REQUIREMENTS:
+      1. Return number as string when get number.
+      2. Return Fizz when get number divisible by 3.
+      3. Return Buzz when get number divisible by 5.
+      3. Return FizzBuzz when get number divisible by 15.
+     */
+}
