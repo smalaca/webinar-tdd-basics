eeaaca7ccccb2fbacbf483f6b0859f01efba4945
smalaca <ab13.krakow@gmail.com>
2020-07-01 14:24:58 +0200

    FizzBuzz - should return Buzz when five given - GREEN

--- fizz-buzz/src/main/java/com/smalaca/webinar/tdd/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/smalaca/webinar/tdd/fizzbuzz/FizzBuzz.java
@@ -2,10 +2,15 @@ package com.smalaca.webinar.tdd.fizzbuzz;
 
 class FizzBuzz {
     private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
 
     String convert(int number) {
         if (isDivisibleByThree(number)) {
             return FIZZ;
+        }
+
+        if (number % 5 == 0) {
+            return BUZZ;
         }
 
         return String.valueOf(number);
