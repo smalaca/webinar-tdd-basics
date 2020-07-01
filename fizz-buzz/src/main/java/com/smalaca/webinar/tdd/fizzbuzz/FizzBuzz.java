74526320fdb9a7835f0d46de1a422fda3c78b4e0
smalaca <ab13.krakow@gmail.com>
2020-07-01 14:22:21 +0200

    FizzBuzz - should return fizz when three given - GREEN

--- fizz-buzz/src/main/java/com/smalaca/webinar/tdd/fizzbuzz/FizzBuzz.java
+++ fizz-buzz/src/main/java/com/smalaca/webinar/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package com.smalaca.webinar.tdd.fizzbuzz;
 
 class FizzBuzz {
     String convert(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
         return String.valueOf(number);
     }
 }
