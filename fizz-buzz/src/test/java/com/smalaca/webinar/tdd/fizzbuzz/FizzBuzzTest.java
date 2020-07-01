d50fd122bfdf9d8c8715caee1f8cc459a52cfb09
smalaca <ab13.krakow@gmail.com>
2020-07-01 14:53:18 +0200

    FizzBuzz - no TDD driven tests - convert tests to parametrized

--- fizz-buzz/src/test/java/com/smalaca/webinar/tdd/fizzbuzz/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/com/smalaca/webinar/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,11 +1,18 @@
 package com.smalaca.webinar.tdd.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FizzBuzzTest {
     private final FizzBuzz fizzBuzz = new FizzBuzz();
+
     /*
       REQUIREMENTS:
       1. Return number as string when get number.
@@ -13,38 +20,44 @@ class FizzBuzzTest {
       3. Return Buzz when get number divisible by 5.
       3. Return FizzBuzz when get number divisible by 15.
      */
+    @ParameterizedTest
+    @MethodSource("numberAsString")
+    void shouldReturnNumberAsString(int number, String expected) {
+        String actual = fizzBuzz.convert(number);
 
-    @Test
-    void shouldReturnOneAsStringWhenOneGiven() {
-        String actual = fizzBuzz.convert(1);
-
-        assertThat(actual).isEqualTo("1");
+        assertThat(actual).isEqualTo(expected);
     }
 
-    @Test
-    void shouldReturnTwoAsStringWhenTwoGiven() {
-        String actual = fizzBuzz.convert(2);
-
-        assertThat(actual).isEqualTo("2");
+    private static Stream<Arguments> numberAsString() {
+        return Stream.of(
+                Arguments.of(1, "1"),
+                Arguments.of(2, "2"),
+                Arguments.of(11, "11"),
+                Arguments.of(13, "13"),
+                Arguments.of(41, "41")
+        );
     }
 
-    @Test
-    void shouldReturnFizzWhenThreeGiven() {
-        String actual = fizzBuzz.convert(3);
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9, 21})
+    void shouldReturnFizzWhenNumberDivisibleByThreeGiven(int number) {
+        String actual = fizzBuzz.convert(number);
 
         assertThat(actual).isEqualTo("Fizz");
     }
 
-    @Test
-    void shouldReturnBuzzWhenFiveGiven() {
-        String actual = fizzBuzz.convert(5);
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 100})
+    void shouldReturnBuzzWhenNumberDivisibleByFiveGiven(int number) {
+        String actual = fizzBuzz.convert(number);
 
         assertThat(actual).isEqualTo("Buzz");
     }
 
-    @Test
-    void shouldReturnFizzBuzzWhenFifteenGiven() {
-        String actual = fizzBuzz.convert(15);
+    @ParameterizedTest
+    @ValueSource(ints = {15, 45, 150, 315})
+    void shouldReturnFizzBuzzWhenNumberDivisibleByFifteenGiven(int number) {
+        String actual = fizzBuzz.convert(number);
 
         assertThat(actual).isEqualTo("FizzBuzz");
     }
